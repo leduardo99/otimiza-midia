c584b80e8e71c07baff59ceb8b1b25ef981ebdd6
luisnascimento <lalbuquerque@2mundos.net>
2023-08-16 22:31:16 -0300

    feat(mobile navbar): adding items on navbar

--- src/components/MobileMenu/index.tsx
+++ src/components/MobileMenu/index.tsx
@@ -71,31 +71,38 @@ export default function MobileMenu() {
           <ul className="px-5 py-2">
             <li>
               <Link
-                href="/signin"
+                href="#about"
                 className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center"
                 onClick={() => setMobileNavOpen(false)}
               >
-                Sign in
+                Sobre Nós
               </Link>
             </li>
             <li>
               <Link
-                href="/signup"
-                className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2"
+                href="#services"
+                className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center"
                 onClick={() => setMobileNavOpen(false)}
               >
-                <span>Sign up</span>
-                <svg
-                  className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1"
-                  viewBox="0 0 12 12"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                    fill="#999"
-                    fillRule="nonzero"
-                  />
-                </svg>
+                Serviços
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#testimonials"
+                className="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center"
+                onClick={() => setMobileNavOpen(false)}
+              >
+                Clientes
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#testimonials"
+                className="px-4 py-2 bg-primary-2 text-white rounded-md flex items-center justify-center gap-2 transition-colors duration-200"
+                onClick={() => setMobileNavOpen(false)}
+              >
+                <span>Vamos Começar</span>
               </Link>
             </li>
           </ul>
